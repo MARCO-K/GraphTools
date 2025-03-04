0d7d00bbb584805f3f05f2fbe3a043c971ad01cf
Marco Kleinert <marco.kleinert@netz-weise.de>
2025-03-04 11:33:16 +0100

    changed: Remove-GTUserEntitlements to give more detailed control

--- internal/functions/Remove-GTUserEntitlements.ps1
+++ internal/functions/Remove-GTUserEntitlements.ps1
@@ -6,19 +6,33 @@
 .PARAMETER UserUPNs
     Array of user principal names to process
 .EXAMPLE
-    Remove-UserEntitlements -UserUPNs "user1@contoso.com","user2@contoso.com"
+    Remove-UserEntitlements -UserUPNs 'user1@contoso.com','user2@contoso.com'
 #>
 function Remove-GTUserEntitlements
 {
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
     param(
         [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-        [string[]]$UserUPNs
+        [string[]]$UserUPNs,
+        [switch]$removeGroups,
+        [switch]$removeGroupOwners,
+        [switch]$removeLicenses,
+        [switch]$removeServicePrincipals,
+        [switch]$removeAll
     )
 
     begin
     {
         $results = [System.Collections.Generic.List[PSObject]]::new()
+        
+        # check for required scopes
+        $RequieredScopes = @('GroupMember.ReadWrite.All', 'Group.ReadWrite.All', 'Directory.ReadWrite.All', 'RoleManagement.ReadWrite.Directory')
+        $missingScopes = $RequieredScopes | Where-Object { $_ -notin (Get-MgContext).Scopes }
+        if ($missingScopes)
+        {
+            throw "Required scopes are missing: $($missingScopes -join ', ')"
+        }
+        else { Write-PSFMessage -Level Verbose -Message "All required scopes are present" }
 
         # install required modules
         $requiremodules = @('Microsoft.Graph.Authentication', 'Microsoft.Graph.Beta.Groups', 'Microsoft.Graph.Beta.Users', 'Microsoft.Graph.Beta.Applications', 'Microsoft.Graph.Beta.Users.Actions')
@@ -40,138 +54,159 @@ function Remove-GTUserEntitlements
                 }
 
                 # 1. Remove Group Memberships
-                $Groups = Get-MgBetaUserTransitiveMemberOfAsGroup -UserId $User.Id -All -ErrorAction Stop
-                foreach ($Group in $Groups)
+                if ($removeGroups)
                 {
-                    $action = "RemoveGroupMembership"
-                    $output = $outputBase + @{
-                        ResourceName = $Group.DisplayName
-                        ResourceType = "Group"
-                        ResourceId   = $Group.Id
-                        Action       = $action
-                    }
-
-                    try
+                    $Groups = Get-MgBetaUserTransitiveMemberOfAsGroup -UserId $User.Id -All -ErrorAction Stop | Where-Object { $_.GroupTypes -ne 'DynamicMembership' }
+                    foreach ($Group in $Groups)
                     {
-                        if ($PSCmdlet.ShouldProcess($Group.DisplayName, $action))
-                        {
-                            Write-PSFMessage -Level Verbose -Message "Removing user $($User.UserPrincipalName) from group $($Group.DisplayName)"
-                            Remove-MgBetaGroupMemberByRef -GroupId $Group.Id -DirectoryObjectId $User.Id -ErrorAction Stop
-                            $output['Status'] = "Success"
+                        $action = 'RemoveGroupMembership'
+                        $output = $outputBase + @{
+                            ResourceName = $Group.DisplayName
+                            ResourceType = 'Group'
+                            ResourceId   = $Group.Id
+                            Action       = $action
                         }
+
+                        try
+                        {
+                            if ($PSCmdlet.ShouldProcess($Group.DisplayName, $action))
+                            {
+                                Write-PSFMessage -Level Verbose -Message "Removing user $($User.UserPrincipalName) from group $($Group.DisplayName)"
+                                Remove-MgBetaGroupMemberByRef -GroupId $Group.Id -DirectoryObjectId $User.Id -ErrorAction Stop
+                                $output['Status'] = 'Success'
+                            }
+                        }
+                        catch
+                        {
+                            Write-PSFMessage -Level Error -Message "Failed to remove user $($User.UserPrincipalName) from group $($Group.DisplayName)."
+                            $output['Status'] = "Failed: $($_.Exception.Message)"
+                        }
+                        $results.Add([PSCustomObject]$output)
                     }
-                    catch
-                    {
-                        Write-PSFMessage -Level Error -Message "Failed to remove user $($User.UserPrincipalName) from group $($Group.DisplayName)."
-                        $output['Status'] = "Failed: $($_.Exception.Message)"
-                    }
-                    $results.Add([PSCustomObject]$output)
                 }
 
                 # 2. Remove Group Ownerships
-                $OwnedGroups = Get-MgBetaUserOwnedObject -UserId $User.Id -All | 
-                Where-Object { $_.AdditionalProperties.'@odata.type' -eq '#microsoft.graph.group' }
-                
-                foreach ($Group in $OwnedGroups)
+                if ($removeGroupOwners -or $removeAll)
                 {
-                    $action = "RemoveGroupOwnership"
-                    $output = $outputBase + @{
-                        ResourceName = $Group.AdditionalProperties.displayName
-                        ResourceType = "Group"
-                        ResourceId   = $Group.Id
-                        Action       = $action
-                    }
-
-                    try
+                    $OwnedGroups = Get-MgBetaUserOwnedObject -UserId $User.Id -All | 
+                    Where-Object { $_.AdditionalProperties.'@odata.type' -eq '#microsoft.graph.group' }
+                    
+                    foreach ($Group in $OwnedGroups)
                     {
-                        $owners = Get-MgBetaGroupOwner -GroupId $Group.Id -All -ErrorAction Stop
-                        if ($owners.Count -eq 1)
-                        {
-                            Write-PSFMessage -Level Verbose -Message "Skipping last owner ($($User.Id)) of group $($Group.Id)"
-                            $output['Status'] = "Skipped: Last owner"
-                            continue
+                        $action = 'RemoveGroupOwnership'
+                        $output = $outputBase + @{
+                            ResourceName = $Group.AdditionalProperties.displayName
+                            ResourceType = 'Group'
+                            ResourceId   = $Group.Id
+                            Action       = $action
                         }
 
-                        if ($PSCmdlet.ShouldProcess($Group.AdditionalProperties.displayName, $action))
+                        try
                         {
-                            Write-PSFMessage -Level Verbose -Message "Removing user $($User.UserPrincipalName) from groupowner $($Group.DisplayName)"
-                            Remove-MgBetaGroupOwnerByRef -GroupId $Group.Id -DirectoryObjectId $User.Id -ErrorAction Stop
-                            $output['Status'] = "Success"
+                            $owners = Get-MgBetaGroupOwner -GroupId $Group.Id -All -ErrorAction Stop
+                            if ($owners.Count -eq 1)
+                            {
+                                Write-PSFMessage -Level Verbose -Message "Skipping last owner ($($User.Id)) of group $($Group.Id)"
+                                $output['Status'] = 'Skipped: Last owner'
+                                continue
+                            }
+
+                            if ($PSCmdlet.ShouldProcess($Group.AdditionalProperties.displayName, $action))
+                            {
+                                Write-PSFMessage -Level Verbose -Message "Removing user $($User.UserPrincipalName) from groupowner $($Group.DisplayName)"
+                                Remove-MgBetaGroupOwnerByRef -GroupId $Group.Id -DirectoryObjectId $User.Id -ErrorAction Stop
+                                $output['Status'] = 'Success'
+                            }
                         }
+                        catch
+                        {
+                            Write-PSFMessage -Level Error -Message "Failed to remove user $($User.UserPrincipalName) from groupowner $($Group.DisplayName)."
+                            $output['Status'] = "Failed: $($_.Exception.Message)"
+                        }
+                        $results.Add([PSCustomObject]$output)
                     }
-                    catch
-                    {
-                        Write-PSFMessage -Level Error -Message "Failed to remove user $($User.UserPrincipalName) from groupowner $($Group.DisplayName)."
-                        $output['Status'] = "Failed: $($_.Exception.Message)"
-                    }
-                    $results.Add([PSCustomObject]$output)
                 }
 
                 # 3. Remove Licenses
-                $licenses = Get-MgBetaUserLicenseDetail -UserId $User.Id -ErrorAction Stop
-                if ($licenses)
+                if ($removeLicenses -or $removeAll)
                 {
-                    $action = "RemoveLicenses"
-                    $output = $outputBase + @{
-                        ResourceName = "Licenses"
-                        ResourceType = "License"
-                        ResourceId   = ($licenses.SkuId -join ", ")
-                        Action       = $action
-                    }
-
-                    try
+                    $licenses = Get-MgBetaUserLicenseDetail -UserId $User.Id -ErrorAction Stop
+                    if ($licenses)
                     {
-                        if ($PSCmdlet.ShouldProcess($UPN, $action))
-                        {
-                            Write-PSFMessage -Level Verbose -Message "Removing licenses from user $($User.UserPrincipalName)"
-                            Set-MgBetaUserLicense -UserId $User.Id -AddLicenses @() -RemoveLicenses @($licenses.SkuId) -ErrorAction Stop
-                            $output['Status'] = "Success"
+                        $action = 'RemoveLicenses'
+                        $output = $outputBase + @{
+                            ResourceName = 'Licenses'
+                            ResourceType = 'License'
+                            ResourceId   = ($licenses.SkuPartNumber -join ', ')
+                            Action       = $action
                         }
+
+                        try
+                        {
+                            if ($PSCmdlet.ShouldProcess($UPN, $action))
+                            {
+                                Write-PSFMessage -Level Verbose -Message "Removing licenses from user $($User.UserPrincipalName)"
+                                Set-MgBetaUserLicense -UserId $User.Id -AddLicenses @() -RemoveLicenses @($licenses.SkuId) -ErrorAction Stop
+                                $output['Status'] = 'Success'
+                            }
+                        }
+                        catch
+                        {
+                            Write-PSFMessage -Level Error -Message "Failed to remove licenses from user $($User.UserPrincipalName)."
+                            $output['Status'] = "Failed: $($_.Exception.Message)"
+                        }
+                        $results.Add([PSCustomObject]$output)
                     }
-                    catch
-                    {
-                        Write-PSFMessage -Level Error -Message "Failed to remove licenses from user $($User.UserPrincipalName)."
-                        $output['Status'] = "Failed: $($_.Exception.Message)"
-                    }
-                    $results.Add([PSCustomObject]$output)
                 }
 
                 # 4. Remove Service Principal Ownerships
-                $servicePrincipals = Get-MgBetaServicePrincipal -Filter "owners/any(o:o/id eq '$($User.Id)')" -All
-                foreach ($sp in $servicePrincipals)
+                if ($removeServicePrincipals -or $removeAll)
                 {
-                    $action = "RemoveServicePrincipalOwnership"
-                    $output = $outputBase + @{
-                        ResourceName = $sp.DisplayName
-                        ResourceType = "ServicePrincipal"
-                        ResourceId   = $sp.Id
-                        Action       = $action
-                    }
-
-                    try
+                    $servicePrincipals = Get-MgBetaServicePrincipal -Filter "owners/`$count eq 1" -CountVariable CountVar -Property 'id,displayName,owners' -ConsistencyLevel 'eventual'
+                    
+                    if ($global:CountVar -gt 0)
                     {
-                        if ($PSCmdlet.ShouldProcess($sp.DisplayName, $action))
+
+                        foreach ($sp in $servicePrincipals)
                         {
-                            Write-PSFMessage -Level Verbose -Message "Removing user $($User.UserPrincipalName) from service principal $($sp.DisplayName)"
-                            Remove-MgBetaServicePrincipalOwnerByRef -ServicePrincipalId $sp.Id -DirectoryObjectId $User.Id -ErrorAction Stop
-                            $output['Status'] = "Success"
+                            $action = 'RemoveServicePrincipalOwnership'
+                            $output = $outputBase + @{
+                                ResourceName = $sp.DisplayName
+                                ResourceType = 'ServicePrincipal'
+                                ResourceId   = $sp.Id
+                                Action       = $action
+                            }
+
+                            try
+                            {
+                                if ($PSCmdlet.ShouldProcess($sp.DisplayName, $action))
+                                {
+                                    Write-PSFMessage -Level Verbose -Message "Removing user $($User.UserPrincipalName) from service principal $($sp.DisplayName)"
+                                    Remove-MgBetaServicePrincipalOwnerByRef -ServicePrincipalId $sp.Id -DirectoryObjectId $User.Id -ErrorAction Stop
+                                    $output['Status'] = 'Success'
+                                }
+                            }
+                            catch
+                            {
+                                Write-PSFMessage -Level Error -Message "Failed to remove user $($User.UserPrincipalName) from service principal $($sp.Id)."
+                                $output['Status'] = "Failed: $($_.Exception.Message)"
+                            }
+                            $results.Add([PSCustomObject]$output)
                         }
                     }
-                    catch
+                    else
                     {
-                        Write-PSFMessage -Level Error -Message "Failed to remove user $($User.UserPrincipalName) from service principal $($sp.Id)."
-                        $output['Status'] = "Failed: $($_.Exception.Message)"
+                        Write-PSFMessage -Level Verbose -Message "No service principals found for user $($User.UserPrincipalName)"
                     }
-                    $results.Add([PSCustomObject]$output)
                 }
             }
             catch
             {
                 $results.Add([PSCustomObject]($outputBase + @{
-                            ResourceName = "UserLookup"
-                            ResourceType = "User"
+                            ResourceName = 'UserLookup'
+                            ResourceType = 'User'
                             ResourceId   = $null
-                            Action       = "UserRetrieval"
+                            Action       = 'UserRetrieval'
                             Status       = "Failed: $($_.Exception.Message)"
                         }))
             }
